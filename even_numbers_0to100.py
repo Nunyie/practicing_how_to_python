2cd6840deefbddba1551a9ba042470a66bd13f62
RoswellBunag <you@example.comroswellbunag05@gmail.com>
2025-03-13 19:51:52 +0800

    Even numbers 0-100

---
+++ even_numbers_0to100.py
@@ -0,0 +1,8 @@
+#Prog9
+#Even numbers from 0-100
+#Nunyie
+print("This code prints out the even numbers from 0-100.")
+           
+for number in range(0, 101):
+    if number %2 == 0:
+        print(number)
