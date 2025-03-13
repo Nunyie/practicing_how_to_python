37b43edc641601f1784c59531935790b882700c3
RoswellBunag <you@example.comroswellbunag05@gmail.com>
2025-03-13 20:54:25 +0800

    quotient of 2 inputs rounded to the nearest whole number

---
+++ quotient_of_2_inputs_whole_number.py
@@ -0,0 +1,18 @@
+#Prog4
+#Quotient no dp
+#Nunyie
+print("This code produces the quotient between 2 numbers")
+print("Output is rounded to 0 decimal places.")
+def valid_input(u):
+    while True:
+        try:
+            return float(input(u))
+        except ValueError:
+            print("Please enter a number")
+            
+num1 = valid_input("First Number = ")
+num2 = valid_input("Second Number = ")
+
+variable = int(num1/num2)
+answer = round(variable, 0)
+print("The quotient of both numbers =", p)
