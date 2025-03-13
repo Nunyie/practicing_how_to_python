2c22092fd42d1c320763524a642384601062debd
RoswellBunag <you@example.comroswellbunag05@gmail.com>
2025-03-13 21:24:22 +0800

    numbers from 0-100 ending in 5s or 0s

---
+++ numbers_from_0to100_ending_in_5s_or_0s.py
@@ -0,0 +1,8 @@
+# Prog9
+# ends with 0/5
+# Nunyie
+print("This code prints out numbers that ends in 0 or 5 from 0-100.")
+           
+for number in range(0, 101):
+    if number %5 == 0 or number %10 ==0:
+        print(number)
