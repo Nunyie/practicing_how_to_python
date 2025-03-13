02a14320673bd9832331ed56aa92c7b15bf392ff
RoswellBunag <you@example.comroswellbunag05@gmail.com>
2025-03-13 21:22:49 +0800

    remainder between 2 inputs

--- remainder_between_2_inputs.py
+++ remainder_between_2_inputs.py
@@ -1,8 +1,8 @@
-#Prog4
-#Quotient no dp
+#Prog5
+#Remainder
 #Nunyie
-print("This code produces the quotient between 2 numbers")
-print("Output is rounded to 0 decimal places.")
+print("This code produces the remainder the first number is divided by the second.")
+
 def valid_input(u):
     while True:
         try:
@@ -13,6 +13,5 @@ def valid_input(u):
 num1 = valid_input("First Number = ")
 num2 = valid_input("Second Number = ")
 
-variable = int(num1/num2)
-answer = round(variable, 0)
-print("The quotient of both numbers =", answer)
+remainder = num1 % num2
+print("The remainder is =", remainder)
