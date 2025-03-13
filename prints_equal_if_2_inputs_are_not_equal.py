3c245dce62fc16fb93fbc030bec2e109dbc62bd8
RoswellBunag <you@example.comroswellbunag05@gmail.com>
2025-03-13 20:15:48 +0800

    Prints not equal when first num is not equal to second number

---
+++ prints_equal_if_2_inputs_are_not_equal.py
@@ -0,0 +1,19 @@
+#Prog2
+#NotEqual
+#Nunyie
+print("This code tells when 2 entered values are not equal.")
+def valid_input(u):
+    while True:
+        try:
+            return float(input(u))
+        except ValueError:
+            print("Please enter a Number")
+
+num1=valid_input("First Number = ")
+num2=valid_input("Second Number = ")
+
+if num1 != num2:
+    print("Both Number are not equal.")
+    
+else:
+    print("Numbers entered is equal")
