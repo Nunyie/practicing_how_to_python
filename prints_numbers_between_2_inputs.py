2babda779ca5f3f1d564e890b79739927613ed61
RoswellBunag <you@example.comroswellbunag05@gmail.com>
2025-03-13 20:17:39 +0800

    Prints the whole numbers in between the 2 inputted numbers

---
+++ prints_numbers_between_2_inputs.py
@@ -0,0 +1,21 @@
+# Prog10
+# inbetween
+# Nunyie
+print("Numbers inbetween input.")
+  
+def valid_input(u):
+    while True:
+        try:
+            return int(input(u))
+        except ValueError:
+            print("Please enter a number")
+            
+num1= valid_input("Input 1 = ")
+num2= valid_input("Input 2 = ")
+
+start = min(num1, num2)
+end = max(num1, num2)
+
+print(f"Numbers between {start} and {end} :")
+for number in range(start + 1, end):
+    print(number)
